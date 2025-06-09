ba4b6b1aeae322fc9b6f98b560fe661909dc7d01
patrik werner <patrik.werner@airolit.com>
2025-06-09 09:52:24 +0200

    fixes git settings attribute path

--- home-manager/_mixins/users/patrik/hosts/bonkywork.nix
+++ home-manager/_mixins/users/patrik/hosts/bonkywork.nix
@@ -3,8 +3,5 @@ with lib.hm.gvariant;
 {
   imports = [ ];
   dconf.settings = { };
-  programs.git = {
-    enable = true;
-    programs.git.userEmail = "patrik.werner@airolit.com";
-  };
+  programs.git.userEmail = "patrik.werner@airolit.com";
 }
