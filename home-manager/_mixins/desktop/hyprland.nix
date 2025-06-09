f6f867c4fd41a733a4689eb7891a0425f44b4d6c
patrik werner <patrikwerner88@hotmail.com>
2025-06-09 22:44:31 +0200

    some theme changes

--- home-manager/_mixins/desktop/hyprland.nix
+++ home-manager/_mixins/desktop/hyprland.nix
@@ -4,13 +4,15 @@ with lib.hm.gvariant;
 
   home = {
     file = {
-      "${config.xdg.configHome}/hypr/hyprland.conf".text = builtins.readFile ./hyprland.conf;
-      "${config.xdg.configHome}/hypr/hyprlock.conf".text = builtins.readFile ./hyprlock.conf;
-      "${config.xdg.configHome}/hypr/hypridle.conf".text = builtins.readFile ./hypridle.conf;
+      "${config.xdg.configHome}/hypr/hyprland.conf".text  = builtins.readFile ./hyprland/hyprland.conf;
+      "${config.xdg.configHome}/hypr/hyprlock.conf".text  = builtins.readFile ./hyprland/hyprlock.conf;
+      "${config.xdg.configHome}/hypr/hypridle.conf".text  = builtins.readFile ./hyprland/hypridle.conf;
+      "${config.xdg.configHome}/rofi/config.rasi".text    = builtins.readFile ./hyprland/config.rasi;
     };
     packages = with pkgs; [
       nemo
-      wofi
+      sysmenu
+      rofi-wayland
       waybar
       wl-clipboard-rs
     ];
