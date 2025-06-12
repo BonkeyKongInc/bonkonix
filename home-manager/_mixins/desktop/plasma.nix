d800c33c69ba8f6960c829c7f1ce405a4a81fa64
patrik werner <patrik.werner@airolit.com>
2025-06-12 10:33:06 +0200

    fixes packages for plasma

--- home-manager/_mixins/desktop/plasma.nix
+++ home-manager/_mixins/desktop/plasma.nix
@@ -1,11 +1,12 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 with lib.hm.gvariant;
 {
   gtk = { };
   services.kdeconnect.enable = true;
 
-  home.file = { };
-  packages = with pkgs; [
-    xclip
-  ]
+  home= { 
+    packages = with pkgs; [
+      xclip
+    ];
+  };
 }
